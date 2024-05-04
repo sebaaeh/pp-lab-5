1b849f71c2b202caf74c59b44c316c4c1364035c
Sebastian Graczyk <seba.graczyk123@gmail.com>
2024-05-04 18:36:01 +0200

    a

---
+++ zad2/Main.java
@@ -0,0 +1,8 @@
+import company.implementations.EmailMessenger;
+
+public class Main {
+    public static void main(String[] args) {
+        EmailMessenger emailMessenger = new EmailMessenger();
+        emailMessenger.sendMessage("email message ");
+    }
+}
